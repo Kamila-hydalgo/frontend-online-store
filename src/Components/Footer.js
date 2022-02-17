78123e5123dcaeed244d0bd97f90d8c257f203f4
André <amorimtorresandre@gmail.com>
2022-02-17 13:44:55 -0300

    ajustando bugs e incluindo header e footer na pág closePurchase

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 class Footer extends Component {
   render() {
     return (
       <div className="logo-footer">
-        <img src="../iconfinder-icon3.svg" alt="logo" />
-        <p>trybeStore</p>
+        <Link
+          to="./"
+          className="logo-footer"
+        >
+          <img src="../iconfinder-icon3.svg" alt="logo" />
+          <p>trybeStore</p>
+        </Link>
       </div>
     );
   }
--- src/pages/ClosePurchase.js
+++ src/pages/ClosePurchase.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { MdPayment } from 'react-icons/md';
 import { RiBarcodeFill } from 'react-icons/ri';
+import Header from '../Components/Hearder';
+import Footer from '../Components/Footer';
 import FinalCard from '../Components/FinalCard';
 
 const uf = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF',
@@ -10,7 +12,21 @@ const uf = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF',
 class ClosePurchase extends Component {
   constructor() {
     super();
-    this.state = { value: 'Estado' };
+    this.state = {
+      value: 'Estado',
+      counterListCart: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.setStateWithLocalStorage();
+  }
+
+  setStateWithLocalStorage = () => {
+    const localStorageCounter = localStorage.getItem('Counter');
+    this.setState({
+      counterListCart: JSON.parse(localStorageCounter),
+    });
   }
 
   handleChange = (event) => {
@@ -18,74 +34,85 @@ class ClosePurchase extends Component {
   }
 
   render() {
-    const { value } = this.state;
+    const { value, counterListCart } = this.state;
     const localStorageItems = JSON.parse(localStorage.getItem('cartItems'));
     return (
-      <form>
-        <fieldset>
-          <legend>Revise seus Produtos</legend>
-          {localStorageItems.map(({ thumbnail, price, title, itemCounter, id }) => (
-            <FinalCard
-              key={ id }
-              image={ thumbnail }
-              price={ price }
-              title={ title }
-              itemCounter={ itemCounter }
+      <div>
+        <Header
+          counterListCart={ counterListCart }
+        />
+        <form>
+          <fieldset>
+            <legend>Revise seus Produtos</legend>
+            {localStorageItems.map(({ thumbnail, price, title, itemCounter, id }) => (
+              <FinalCard
+                key={ id }
+                image={ thumbnail }
+                price={ price }
+                title={ title }
+                itemCounter={ itemCounter }
+              />
+            ))}
+            <p>Total: R$</p>
+          </fieldset>
+          <fieldset>
+            <legend>Informações do Comprador</legend>
+            <input
+              data-testid="checkout-fullname"
+              type="text"
+              placeholder="Nome Completo"
+              required
             />
-          ))}
-          <p>Total: R$</p>
-        </fieldset>
-        <fieldset>
-          <legend>Informações do Comprador</legend>
-          <input
-            data-testid="checkout-fullname"
-            type="text"
-            placeholder="Nome Completo"
-            required
-          />
-          <input data-testid="checkout-cpf" type="text" placeholder="CPF" required />
-          <input data-testid="checkout-email" type="email" placeholder="Email" required />
-          <input
-            data-testid="checkout-phone"
-            type="tel"
-            placeholder="Telefone"
-            required
-          />
-          <input data-testid="checkout-cep" type="text" placeholder="CEP" required />
-          <input
-            data-testid="checkout-address"
-            type="text"
-            placeholder="Endereço"
-            required
-          />
-          <input type="text" placeholder="Complemento" required />
-          <input type="text" placeholder="Número" required />
-          <input type="text" placeholder="Cidade" required />
-          <label htmlFor="state">
-            <select id="state" value={ value } onChange={ this.handleChange }>
-              <option value="Estado" disabled>Estado</option>
-              {
-                uf.map((elem) => <option key={ elem }>{ elem }</option>)
-              }
-            </select>
-          </label>
-        </fieldset>
-        <fieldset>
-          <legend>Método de Pagamento</legend>
-          <input type="radio" name="payment" required />
-          <RiBarcodeFill />
-          <input type="radio" name="payment" required />
-          Visa
-          <MdPayment />
-          <input type="radio" name="payment" required />
-          MasterCard
-          <MdPayment />
-          <input type="radio" name="payment" required />
-          Elo
-          <MdPayment />
-        </fieldset>
-        <button type="submit">Comprar</button>
-      </form>
+            <input data-testid="checkout-cpf" type="text" placeholder="CPF" required />
+            <input
+              data-testid="checkout-email"
+              type="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              data-testid="checkout-phone"
+              type="tel"
+              placeholder="Telefone"
+              required
+            />
+            <input data-testid="checkout-cep" type="text" placeholder="CEP" required />
+            <input
+              data-testid="checkout-address"
+              type="text"
+              placeholder="Endereço"
+              required
+            />
+            <input type="text" placeholder="Complemento" required />
+            <input type="text" placeholder="Número" required />
+            <input type="text" placeholder="Cidade" required />
+            <label htmlFor="state">
+              <select id="state" value={ value } onChange={ this.handleChange }>
+                <option value="Estado" disabled> Estado </option>
+                {
+                  uf.map((elem) => <option key={ elem }>{ elem }</option>)
+                }
+              </select>
+            </label>
+          </fieldset>
+          <fieldset>
+            <legend>Método de Pagamento</legend>
+            <input type="radio" name="payment" required />
+            <RiBarcodeFill />
+            <input type="radio" name="payment" required />
+            Visa
+            <MdPayment />
+            <input type="radio" name="payment" required />
+            MasterCard
+            <MdPayment />
+            <input type="radio" name="payment" required />
+            Elo
+            <MdPayment />
+          </fieldset>
+          <button type="submit">Comprar</button>
+        </form>
+        <Footer />
+      </div>
     );
   }
 }
